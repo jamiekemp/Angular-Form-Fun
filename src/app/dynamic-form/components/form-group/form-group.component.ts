62bf3d74d4b37810c63867d3d0437f242375cea9
Jamie Kemp <jamiekemp@gmail.com>
2018-02-06 22:51:45 +0000

    CLI create form group component (in progress) | deleted styleUrls property

--- src/app/dynamic-form/components/form-group/form-group.component.ts
+++ src/app/dynamic-form/components/form-group/form-group.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form-group',
-  templateUrl: './form-group.component.html',
-  styleUrls: ['./form-group.component.scss']
+  templateUrl: './form-group.component.html'
 })
 export class FormGroupComponent implements OnInit {
 
